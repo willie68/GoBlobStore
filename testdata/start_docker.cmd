8ee0a97bbdcf6eb6675ffa0c06f2b896160d89b3
Klaas, Wilfried <w.klaas@gmx.de>
2021-11-12 17:04:19 +0100

    starting minio in docker

---
+++ testdata/start_docker.cmd
@@ -0,0 +1,1 @@
+docker run -d --name minio --restart always -p 9002:9002 -p 9001:9001 -v E:\TEMP\minio\stg:/data -e "MINIO_ROOT_USER=D9Q2D6JQGW1MVCC98LQL" -e "MINIO_ROOT_PASSWORD=LDX7QHY/IsNiA9DbdycGMuOP0M4khr0+06DKrFAr" minio/minio server /data --address ":9002" --console-address ":9001"
