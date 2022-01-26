37cb2e163449e14351f853f4bcec8efe2c818857
Klaas, Wilfried <w.klaas@gmx.de>
2022-01-26 10:45:14 +0100

    Moving dockerfile to build/package
    changing to alpine3.15

--- deployments/buildDocker.cmd
+++ deployments/buildDocker.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo building service
-docker build --tag mcs/go-blob-store:V1 ./
+docker build -f ./build/package/Dockerfile --tag mcs/go-blob-store:V1 ./
 
 docker run --name GoBlobStore -v h:/temp/blbstg:/data/storage -p 8443:8443 mcs/go-blob-store:V1
