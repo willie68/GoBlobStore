9c711d99a2d9f1b009e0d25301355a0df174d7fb
Wilfried Klaas <w.klaas@gmx.de>
2022-02-27 07:23:12 +0100

    generate without path

--- build/pigeon/generate.cmd
+++ build/pigeon/generate.cmd
@@ -3,4 +3,4 @@ cls
 echo ***** generate
 pigeon -o pkg/model/query/parser.go build/pigeon/parser.peg
 echo ***** test
-E:\SPRACHEN\go\bin\go.exe test -timeout 180s -run ^TestQParse$ github.com/willie68/GoBlobStore/pkg/model/query -v
+go.exe test -timeout 180s -run ^TestQParse$ github.com/willie68/GoBlobStore/pkg/model/query -v
