7eb8a344105039072ef5fc6a114e6a97f7f1a848
Wilfried Klaas <w.klaas@gmx.de>
2023-01-12 12:39:59 +0100

    #31 async get size for tenant.

--- internal/dao/simplefile/simplefiletenantmanager_test.go
+++ internal/dao/simplefile/simplefiletenantmanager_test.go
@@ -3,12 +3,14 @@ package simplefile
 import (
 	"errors"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/willie68/GoBlobStore/internal/config"
 	"github.com/willie68/GoBlobStore/internal/dao/interfaces"
+	"github.com/willie68/GoBlobStore/pkg/model"
 )
 
 func initTenantTest(t *testing.T) {
@@ -81,7 +83,7 @@ func TestSimplefileTenantManager(t *testing.T) {
 	ast.True(ok)
 
 	size := dao.GetSize(tenant)
-	ast.Equal(int64(0), size)
+	ast.True(size <= 0)
 
 	_, err = dao.RemoveTenant(tenant)
 	ast.Nil(err)
@@ -137,4 +139,66 @@ func TestSimplefileTenantManagerConfig(t *testing.T) {
 	ast.Equal(cfn.Backup.Storageclass, cfn2.Backup.Storageclass)
 	ast.Equal(cfn.Backup.Properties["accessKey"], cfn2.Backup.Properties["accessKey"])
 	ast.Equal(cfn.Backup.Properties["secretKey"], cfn2.Backup.Properties["secretKey"])
+}
+
+func TestSize(t *testing.T) {
+	initTenantTest(t)
+
+	ast := assert.New(t)
+
+	tntdao := SimpleFileTenantManager{
+		RootPath: rootpath,
+	}
+	dao := SimpleFileBlobStorageDao{
+		RootPath: rootpath,
+		Tenant:   tenant,
+	}
+
+	err := dao.Init()
+	ast.Nil(err)
+	err = tntdao.Init()
+	ast.Nil(err)
+
+	time.Sleep(1 * time.Second)
+
+	tenants := make([]string, 0)
+	err = tntdao.GetTenants(func(t string) bool {
+		tenants = append(tenants, t)
+		return true
+	})
+
+	ast.Nil(err)
+	ast.Equal(0, len(tenants))
+
+	err = tntdao.AddTenant(tenant)
+	ast.Nil(err)
+	ast.True(tntdao.HasTenant(tenant))
+
+	size := tntdao.GetSize(tenant)
+	ast.True(size <= 0)
+
+	b := model.BlobDescription{
+		StoreID:       "MCS",
+		TenantID:      "MCS",
+		ContentLength: 22,
+		ContentType:   "text/plain",
+		CreationDate:  time.Now().UnixMilli(),
+		Filename:      "test.txt",
+		LastAccess:    time.Now().UnixMilli(),
+		Retention:     180000,
+		Properties:    make(map[string]interface{}),
+	}
+	b.Properties["X-user"] = []string{"Hallo", "Hallo2"}
+	b.Properties["X-retention"] = []int{123456}
+	b.Properties["X-tenant"] = "MCS"
+
+	r := strings.NewReader("this is a blob content")
+	id, err := dao.StoreBlob(&b, r)
+	ast.Nil(err)
+	ast.NotNil(id)
+	ast.Equal(id, b.BlobID)
+	time.Sleep(90 * time.Second)
+
+	size = tntdao.GetSize(tenant)
+	ast.True(size > 0)
 }
