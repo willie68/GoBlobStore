80852073d3a6f35291a65c17f8c183b03ee8aaad
Wilfried Klaas <w.klaas@gmx.de>
2023-02-05 17:36:37 +0100

    #33 next stpes

--- internal/services/extractor/tika_test.go
+++ internal/services/extractor/tika_test.go
@@ -2,14 +2,42 @@ package extractor
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/willie68/GoBlobStore/internal/config"
+	"github.com/willie68/GoBlobStore/internal/services/interfaces/mocks"
+	"github.com/willie68/GoBlobStore/pkg/model"
 )
 
 func TestTikaExtractorTxt(t *testing.T) {
 	ast := assert.New(t)
 
 	ext := NewExtractor("tika")
-	_, ok := ext.(*Tika)
+	tk, ok := ext.(*Tika)
 	ast.True(ok)
+
+	stg := &mocks.BlobStorage{}
+	bd := &model.BlobDescription{
+		StoreID:       "mcs",
+		ContentLength: 1234,
+		ContentType:   "text/plain",
+		CreationDate:  time.Now().Unix(),
+		Filename:      "test.txt",
+		TenantID:      "mcs",
+		BlobID:        "12345678",
+		LastAccess:    time.Now().Unix(),
+		Retention:     0,
+	}
+	stg.EXPECT().HasBlob(mock.Anything).Return(true, nil)
+	stg.EXPECT().GetBlobDescription(mock.Anything).Return(bd, nil)
+	cfg := config.Extractor{}
+	err := tk.Init(cfg, stg)
+	ast.Nil(err)
+
+	bdm, err := tk.Metadata("12345678")
+	ast.Nil(err)
+	ast.NotNil(bdm)
+	ast.Equal("test.txt", bdm["filename"])
 }
